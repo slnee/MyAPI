ca8f0b6431a9f415e23d19b5176047a0f1c5487e
student <student@Chalon08.local>
2026-09-09 16:04:50 +0200

    Fix(json): fix json file

---
+++ docs/README.md
@@ -0,0 +1,11 @@
+# MyAPI
+Node
+Express
+Postgres
+Prisma ?
+Redis
+JWT ou BCrypt
+Swagger
+AWS
+
+postgres://1336660a522b4293104034689c44b6152805039aeae8d55c186fe415682ccb1d:sk_5cugF7KpkG6GQTUJhMyu5@pooled.db.prisma.io:5432/postgres?sslmode=require
--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
-const express = require('express'); //import exprseess
+import express from 'express'; //import exprseess
 const app = express() //créer l'app
+
 
 app.get('/', (req, res) => {//envoie des information au frontend de base
 //req = ce que veut l'utilisateur / res = utilise pour renvoyer la reponse
 res.send('/ route work')
 }) 
 
+app.get('/connected', (req, res) => {
+res.send('/connected route work')
+}) 
+
+app.get('/log_in', (req, res) => {
+res.send('/log_in route work')
+})
+
+app.get('/wrong_input', (req, res) => {
+res.send('/wrong_input route work')
+}) 
+
+app.get('/connected/:nom', (req, res) => {
+res.send('/connected/:nom route work')
+}) 
 
 app.listen(3000, () => { //strat listening
     console.log('the server is running')
